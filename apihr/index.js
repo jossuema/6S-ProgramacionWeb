09eeb35e9f98f118a33b73009a3a842c2f3f057f
jossuema <mmalla1@outlook.com>
2024-11-06 10:14:43 -0500

    Adding Middleware

--- apihr/index.js
+++ apihr/index.js
@@ -1,6 +1,49 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
+const session = require('express-session');
+const csrf = require('csurf');
+const winston = require('winston');
+const helmet = require('helmet');
+const app = express();
+
+const logger = winston.createLogger({
+    level: 'info', // Nivel mínimo de log (info, warn, error)
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level.toUpperCase()}] - ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(), // Log en la consola
+        new winston.transports.File({ filename: 'logs/activities.log' }) // Log en archivo
+    ]
+});
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100, // límite de 100 solicitudes por IP en 15 minutos
+    message: 'Demasiadas solicitudes desde esta IP. Por favor, inténtalo de nuevo más tarde.',
+    onLimitReached: (req, res, options) => {
+        logger.warn(`Límite de solicitudes excedido desde la IP: ${req.ip}`);
+    }
+});
+
+app.use(session({
+    secret: 'VODCdHTl',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+const csrfProtection = csrf();
+app.use(csrfProtection);
+
+app.use(xss());
+
+app.use(limiter);
 
 app.use(cors());
 
